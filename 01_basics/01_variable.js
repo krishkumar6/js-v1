cb651433fe421dca8e7a98afe2dbba503ea72242
krishkumar6 <krishkumarlaw@gmail.com>
2024-10-06 18:08:54 +0530

    #container congf done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=1443 //once a value is defined or assigned it can't change
+
+let accountEmail="krish@gmail.com" //we can change it
+
+var accountPassword="1234" //we can change it
+
+/* we prefer let over var because of issue in block scope and function scope*/
+
+accountCity="jaipur" //we can use without let an var
+
+let accountState; //javascript consider undefined value if you not assigned
+
+console.log(accountId); //use in print
+
+console.table([accountEmail,accountId,accountState,accountCity]); //it use to print multiple statement in single syntax
+
+/* note=> In word second sentence ka first letter always written in capital letter 
+       Ex=> accountEmail */
