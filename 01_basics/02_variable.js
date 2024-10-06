c4168527987c8164beb8504b855bf08c509e621c
krishkumar6 <krishkumarlaw@gmail.com>
2024-10-06 23:36:18 +0530

    #container confg done

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,27 @@
+"use strict" //  treat all Js code in a new version
+
+//alert(3 + 3) // this is give error because we in nodejs not a console in browser
+
+let name="krish" //string
+let age=18     //number
+let isLoggedIn=false //bool
+
+/*
+data types
+=>number => 2 to power 53
+=>bigint =>
+=>string=>" "
+=>boolean => false/true
+=>null=>standalone value
+=>undefined =>
+=>symbol=>unique
+*/
+
+//object
+
+console.log(typeof null) // data type => object
+console.log(typeof undefined) //data type =>undefined
+
+
+
+
